a293727e41130e830b22ed25fe93a06084eb262d
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-10-17 20:59:42 +0300

    Updated routs

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,15 +1,15 @@
 class Booking < ActiveRecord::Base
   belongs_to :boards_ids, class_name: 'Board', foreign_key: :board_id
 
-  # validates_presence_of :start_time, :end_time, :board_id
+  validates_presence_of :start_time, :end_time, :board_id
 
+  validate :no_booking_overlap
 
+  def no_booking_overlap
+    if Booking.where("(? BETWEEN start_time AND end_time OR ? BETWEEN start_time AND end_time) AND board_id = ?",
+                     self.start_time, self.end_time, self.board_id).any?
+      errors.add(:end_time, 'it overlaps another booking for this table')
+    end
+  end
 
-  # def end_time_after_start_time
-  #   return if end_time.blank? || start_time.blank?
-  #
-  #   if end_time < start_time
-  #     errors.add(:end_time, 'must be after the start time')
-  #   end
-  # end
 end
