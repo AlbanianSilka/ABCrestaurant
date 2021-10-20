4c0746d665f7eb6c65fd7215c2370fbf689cf942
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-10-20 04:09:33 +0300

    Added method for booking destroying after 24 hours.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,15 +1,8 @@
 class Booking < ActiveRecord::Base
   belongs_to :boards_ids, class_name: 'Board', foreign_key: :board_id
 
-  validates_presence_of :start_time, :end_time, :board_id
-
-  validate :no_booking_overlap
-
-  def no_booking_overlap
-    if Booking.where("(? BETWEEN start_time AND end_time OR ? BETWEEN start_time AND end_time) AND board_id = ?",
-                     self.start_time, self.end_time, self.board_id).any?
-      errors.add(:end_time, 'it overlaps another booking for this table')
-    end
+  def delete_old_booking
+      self.class.destroy(id)
   end
 
 end
