91af046fa3c9b91715e0be905d6714d0723f5b7f
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-10-20 19:42:39 +0300

    Created special service for creating restaurant workday, at this moment borders is not connected to the UI but at least now we can do it through the code.

---
+++ app/services/daytime_service.rb
@@ -0,0 +1,30 @@
+class DaytimeService
+  def initialize(booking)
+    @booking = booking
+  end
+  # in this part we declare hours and minutes, if you want to change the minimum start of the working day - write another number
+
+  def create_workday(min_hours, max_hours)
+    graph_time = []
+
+    until min_hours > max_hours
+      graph_time.push("#{min_hours}")
+      min_hours += 1
+    end
+
+    graph_all = []
+    minute = 0
+
+    graph_time.each do |el|
+      while minute < 60
+        graph_all.push("#{el}" ":" "#{minute}")
+        minute += 15
+      end
+      if minute == 60
+        minute = 0
+      end
+    end
+
+    graph_all
+  end
+end
