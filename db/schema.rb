a8c8d6fadb9ba239682519a58f45d203263ca952
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-10-21 00:38:52 +0300

    Updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_16_194451) do
+ActiveRecord::Schema.define(version: 2021_10_20_210340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_10_16_194451) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "board_id"
+    t.text "email"
     t.index ["board_id"], name: "index_bookings_on_board_id"
   end
 
