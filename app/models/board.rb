00cccef490b3e1df3e3ae10cc41593eb0823e595
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-10-20 04:09:00 +0300

    Added check booking method into the board model + updated schema.rb

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,4 +2,12 @@ class Board < ActiveRecord::Base
   validates_presence_of :board_number, :board_seats
 
   has_many :bookings
+
+  after_initialize :check_booking
+
+  def check_booking
+    if self.bookings.length == 0
+      self.booked = false
+    end
+  end
 end
