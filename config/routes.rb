b94418300461668e65cf1cb6f43350edef074db5
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-10-23 22:14:27 +0300

    Added dishes routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :boards do
     resources :bookings, shallow: true
   end
+
+  resources :dishes
 
   get 'about', to: "about#index"
   get 'welcome/index'
